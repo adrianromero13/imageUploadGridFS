0d322bf232d1058beddfd7da6cf9539a839a3a22
Adrian Romero <radrian031613@gmail.com>
2020-07-21 13:43:25 -0700

    set up middlewares for grid-fs

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 
 // initialize app variable
 const app = express();
 
-// set up view engine
+// Middlewares
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));  // used to create delete request using form
 app.set('view engine', 'ejs');
 
 // set up routes
@@ -19,7 +28,7 @@ const PORT = 5000;
 app.listen(PORT, () => console.log(`server starte on PORT: ${PORT}`));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/girdfsuploads',
-  {
+{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
